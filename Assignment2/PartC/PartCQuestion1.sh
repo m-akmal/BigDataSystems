337287ef46dba700ab6c27a064345f03b91b8fb5
pavankm <pavank@protonmail.com>
2017-10-28 00:07:32 -0500

    changed location of flink to match assignment web page

--- Assignment2/PartC/PartCQuestion1.sh
+++ Assignment2/PartC/PartCQuestion1.sh
@@ -1,1 +1,1 @@
-/home/ubuntu/flink-1.3.2/bin/flink run -c org.myorg.quickstart.PartCQuestion1 ./target/quickstart-0.1.java
+~/software/flink-1.3.2/bin/flink run -c org.myorg.quickstart.PartCQuestion1 ./target/quickstart-0.1.java
