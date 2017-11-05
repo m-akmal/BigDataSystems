1cd43919a7e496161a438d37e173914110433889
Rohit Damkondwar <rohitsdamkondwar@gmail.com>
2017-11-05 14:24:58 -0600

    Adding resource_monitor and kubernetes steps

---
+++ Project/src/resource_monitor.py
@@ -0,0 +1,33 @@
+import os
+import psutil
+import time
+import datetime
+import signal
+import sys
+
+#pid = os.getpid()
+PROCESS_NAME = "python"
+#PROG_NAME = "cifar10_train.py"
+PROG_NAME = "resource_monitor.py"
+SLEEP_DURATION=5
+
+logfile = open('resource_usage.log', 'a')
+def signal_handler(signal, frame):
+            print('Exiting')
+            logfile.close()
+            sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+while True:
+    time.sleep(1)
+    if int(time.time())%SLEEP_DURATION is not 0:
+        continue
+    print time.time(), int(time.time())
+    pid = os.getpid()
+    for proc in psutil.process_iter():
+        if proc.name() == PROCESS_NAME and proc.cmdline()[-1].endswith(PROG_NAME):
+            l = [str(time.time()), str(datetime.datetime.now()), str(proc), str(proc.cmdline()), str(proc.cpu_times()), str(proc.memory_info())]
+            line = ' '.join(l)
+            logfile.write("%s\n" % str(line))
+            logfile.flush()
