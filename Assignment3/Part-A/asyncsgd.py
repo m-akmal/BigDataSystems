7d358606b9aa85ade684b47979c1ee8d8a8c679d
pavankm <pavank@protonmail.com>
2017-12-01 13:49:36 -0600

    changed files for different clients

--- Assignment3/Part-A/asyncsgd.py
+++ Assignment3/Part-A/asyncsgd.py
@@ -8,6 +8,13 @@ num_features = 33762578
 eta = 0.01
 num_iterations = 1000
 
+files = {
+    0: ['/home/ubuntu/rohit/tf/syncsgd/data/criteo-tfr/tfrecords00', '/home/ubuntu/rohit/tf/syncsgd/data/criteo-tfr/tfrecords01', '/home/ubuntu/rohit/tf/syncsgd/data/criteo-tfr/tfrecords02', '/home/ubuntu/rohit/tf/syncsgd/data/criteo-tfr/tfrecords03', '/home/ubuntu/rohit/tf/syncsgd/data/criteo-tfr/tfrecords04'],
+    1: ['/home/ubuntu/rohit/tf/syncsgd/data/criteo-tfr/tfrecords05', '/home/ubuntu/rohit/tf/syncsgd/data/criteo-tfr/tfrecords06', '/home/ubuntu/rohit/tf/syncsgd/data/criteo-tfr/tfrecords07', '/home/ubuntu/rohit/tf/syncsgd/data/criteo-tfr/tfrecords08', '/home/ubuntu/rohit/tf/syncsgd/data/criteo-tfr/tfrecords09'],
+    2: ['/home/ubuntu/rohit/tf/syncsgd/data/criteo-tfr/tfrecords10', '/home/ubuntu/rohit/tf/syncsgd/data/criteo-tfr/tfrecords11', '/home/ubuntu/rohit/tf/syncsgd/data/criteo-tfr/tfrecords12', '/home/ubuntu/rohit/tf/syncsgd/data/criteo-tfr/tfrecords13', '/home/ubuntu/rohit/tf/syncsgd/data/criteo-tfr/tfrecords14'],
+    3: ['/home/ubuntu/rohit/tf/syncsgd/data/criteo-tfr/tfrecords15', '/home/ubuntu/rohit/tf/syncsgd/data/criteo-tfr/tfrecords16', '/home/ubuntu/rohit/tf/syncsgd/data/criteo-tfr/tfrecords17', '/home/ubuntu/rohit/tf/syncsgd/data/criteo-tfr/tfrecords18', '/home/ubuntu/rohit/tf/syncsgd/data/criteo-tfr/tfrecords19'],
+    4: ['/home/ubuntu/rohit/tf/syncsgd/data/criteo-tfr/tfrecords20', '/home/ubuntu/rohit/tf/syncsgd/data/criteo-tfr/tfrecords21', '/home/ubuntu/rohit/tf/syncsgd/data/criteo-tfr/tfrecords22']
+}
 g = tf.Graph()
 
 with g.as_default():
@@ -20,7 +27,7 @@ with g.as_default():
     # here, they emit predefined tensors. however, they can be defined as reader
     # operators as done in "exampleReadCriteoData.py"
     with tf.device("/job:worker/task:%d" % FLAGS.task_index):
-        filename_queue = tf.train.string_input_producer(["/home/ubuntu/rohit/tf/syncsgd/data/criteo-tfr-tiny/tfrecords00"], num_epochs=None)
+        filename_queue = tf.train.string_input_producer(files[FLAGS.task_index], num_epochs=None)
         reader = tf.TFRecordReader()
 
         _, serialized_example = reader.read(filename_queue)
@@ -53,9 +60,10 @@ with g.as_default():
 
 
     with tf.Session("grpc://vm-23-%d:2222" % (FLAGS.task_index+1)) as sess:
-	    cord = tf.train.Coordinator()
-        threads = tf.train.start_queue_runners(sess=sess,  coord=cord)
-        # only one client initializes the variable
+        cord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord=cord)
+        if FLAGS.task_index == 0:
+            sess.run(tf.initialize_all_variables())
         if FLAGS.task_index == 0:
             sess.run(tf.initialize_all_variables())
         for i in range(0, num_iterations):
