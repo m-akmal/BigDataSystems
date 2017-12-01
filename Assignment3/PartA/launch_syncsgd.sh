2cfce40358371f2942935bd8169f9e7718056e6b
pavankm <pavank@protonmail.com>
2017-12-01 14:15:58 -0600

    renaming dir

--- Assignment3/PartA/launch_syncsgd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source tfdefs.sh
-start_cluster startserver.py
-# start multiple clients
-echo "starting sync sgd"
-python synchronoussgd.py 
-sleep 2 # wait for variable to be initialized
